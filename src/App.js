112401eb0ea73a2f99cac1930615a1c381aaa7d9
dhavalpsoni <dhaval.soni.18584@gmail.com>
2020-10-06 08:32:27 -0400

    adding header component and links on home page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import Header from "./components/header/header.component";
 
 // react-router-dom for router.  "Link" for redirect and re-render page from html.  <Link to="/topic">topic</Link>
 // "History" to redirect from JS. props.history.push('/topic');
@@ -17,6 +12,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route exact path="/shop" component={ShopPage}></Route>
