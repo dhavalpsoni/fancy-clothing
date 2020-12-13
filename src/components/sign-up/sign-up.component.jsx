06e15e3f45b15ba641a664ada04cc0397543ea78
dhavalpsoni <dhaval.soni.18584@gmail.com>
2020-12-13 12:20:48 -0500

    sign in and sign up

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
+
+import { auth, createUserProfileDocument } from "../../firebase/firebase.util";
+
+class SignUp extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      displayName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    };
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const { displayName, email, password, confirmPassword } = this.state;
+    if (password !== confirmPassword) {
+      alert("Password not matched");
+      return;
+    }
+    try {
+      const { user } = auth.createUserProfileDocument(email, password);
+      await createUserProfileDocument(user, { displayName });
+
+      this.state = {
+        displayName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
+  };
+  render() {
+    const { displayName, email, password, confirmPassword } = this.state;
+    return (
+      <div className="sign-up">
+        <h2 className="title">Sign Up</h2>
+        <span>Signup with your email and password</span>
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="DisplayName"
+            value={displayName}
+            onChange={this.onChange}
+            label="Display Name"
+            required
+          ></FormInput>
+          <FormInput
+            type="text"
+            name="Email"
+            value={email}
+            onChange={this.onChange}
+            label="Email"
+            required
+          ></FormInput>
+          <FormInput
+            type="text"
+            name="Password"
+            value={password}
+            onChange={this.onChange}
+            label="Password"
+            required
+          ></FormInput>
+          <FormInput
+            type="text"
+            name="ConfirmPassword"
+            value={confirmPassword}
+            onChange={this.onChange}
+            label="Confirm Password"
+            required
+          ></FormInput>
+          <CustomButton type="Submit">Sign Up</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+export default SignUp;
--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -3,20 +3,102 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyBH2gl27Dkav2900M9tOgagI_0OmczeBq0",
-  authDomain: "crwn-db-c6c37.firebaseapp.com",
-  projectId: "crwn-db-c6c37",
-  storageBucket: "crwn-db-c6c37.appspot.com",
-  messagingSenderId: "1001177950303",
-  appId: "1:1001177950303:web:71d367a392d95da1ba93ea",
+  apiKey: "AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14",
+  authDomain: "crwn-db.firebaseapp.com",
+  databaseURL: "https://crwn-db.firebaseio.com",
+  projectId: "crwn-db",
+  storageBucket: "crwn-db.appspot.com",
+  messagingSenderId: "850995411664",
+  appId: "1:850995411664:web:7ddc01d597846f65",
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const getUserCartRef = async (userId) => {
+  const cartsRef = firestore.collection("carts").where("userId", "==", userId);
+  const snapShot = await cartsRef.get();
+
+  if (snapShot.empty) {
+    const cartDocRef = firestore.collection("carts").doc();
+    await cartDocRef.set({ userId, cartItems: [] });
+    return cartDocRef;
+  } else {
+    return snapShot.docs[0].ref;
+  }
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
+
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      unsubscribe();
+      resolve(userAuth);
+    }, reject);
+  });
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select-account" });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
 
 export default firebase;
